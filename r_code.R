0e0ac1e6d0b0b10449eee347c1ddd12e22191cc9
Allen <dallen@middlebury.edu>
2020-04-15 13:45:45 -0400

    additions to r code file

--- r_code.R
+++ r_code.R
@@ -72,7 +72,6 @@ require(bbmle)
   
 }
 
-
 # fit curves to phenology patterns for three elevation classes
 {
   samples <- read_csv('data/drag_sampling.csv') %>% mutate(elevCat = cut(elev,c(0,200,410,1000),c('low','mid','high')))
@@ -184,5 +183,125 @@ require(bbmle)
   
 }
 
+# make plot comparing parameters
+{
+  load(file = 'data/phenology_fits.RData') # see above for code which fit these functions
+  # compare parameters
+  paramcomp <- tibble(
+    elev = c('<200 m','<200 m','200 - 400 m','200 - 400 m','>400 m','>400 m'),
+    when = rep(c('Early','Late'),3),
+    param = c(fitList$low$larva$peak_e[1],fitList$low$larva$peak_l[1],fitList$mid$larva$peak_e[1],fitList$mid$larva$peak_l[1],fitList$high$larva$peak_e[1],fitList$high$larva$peak_l[1]),
+    upper_param = c(fitList$low$larva$peak_e[3],fitList$low$larva$peak_l[3],fitList$mid$larva$peak_e[3],fitList$mid$larva$peak_l[3],fitList$high$larva$peak_e[3],fitList$high$larva$peak_l[3]),
+    lower_param =c(fitList$low$larva$peak_e[2],fitList$low$larva$peak_l[2],fitList$mid$larva$peak_e[2],fitList$mid$larva$peak_l[2],fitList$high$larva$peak_e[2],fitList$high$larva$peak_l[2]),
+  ) %>% 
+    mutate(elev= factor(elev,levels = c('<200 m','200 - 400 m','>400 m')))
+  
+  pdf('figures/peak_param_comp.pdf',width=5,height=4)
+    ylab <- expression(paste('Peak (larvae per 200 ',m^2,')'))
+    paramcomp %>%
+      ggplot(aes(when,param)) +
+      geom_point() +
+      facet_wrap(~elev) +
+      geom_errorbar(aes(ymin=lower_param,ymax=upper_param), width=0.5) +
+      theme_classic() +
+      theme(strip.background = element_rect(color='transparent'),
+            axis.text = element_text(color='black')) +
+      labs(x='',y=ylab) +
+      scale_y_log10(breaks=c(1e-2,1e-1,1,1e1,1e2),
+                    labels = c(.001,0.1,1,10,100))    
+  dev.off()
+}
 
+# larval development and question model
+{
+  # parameters for model
+  params <- list(
+    ovi_m = 243.2, # in degree days base 6 C Rand et al. 2004
+    ovi_sd = 63.1, # in degree days base 6 C Rand et al. 2004
+    ecl_m = 428.5, # in degree days base 11 C Rand et al. 2004
+    ecl_sd = 70.8, # in degree days base 11 C Rand et al. 2004
+    hardening = 21, # in days Daniels et al. 1996
+    start_quest = 10, # in C Ogden et al. 2005
+    max_quest = 25, # in C Ogden et al. 2005
+    quest_slope1 = 0.067, # in 1/C Odgen et al. 2005
+    quest_slope2 = -0.067, # in 1/C Odgen et al. 2005
+    host_find = 0.0207, # in 1/day Odgen et al. 2005
+    mort = 0.006, # in 1/day Odgen et al. 2005
+    diapause = 0.25, # Ogden et al. 2018
+    overwinter_surv = 0.44 # Lindsay et al. 1995
+  )
+  
+  # fake temp to see if it works
+  # tmean <- 17.5*cos(mydays*2*pi/365 - pi) + 7
+ 
+  # takes in daily average temp at leaf litter and parameters
+  # outputs fraction of larvae oviposited, ecolosed, questing on each day
+  larval_quest <- function(tmean, params)
+  {
+    tot_day <- length(tmean)
+    dd6 <- cumsum(ifelse(tmean > 6, tmean - 6, 0))
+    # fraction of cohort oviposited on each day
+    ovi = pnorm(dd6, params$ovi_m, params$ovi_sd) - pnorm(lag(dd6, n = 1, default = 0), params$ovi_m, params$ovi_sd)             
+    
+    # fraction of cohort eclosed on each day
+    ecl <- rep(0, tot_day)
+    for (i in 1:(tot_day-1))
+    {
+      i1 <- i+1
+      dd11 <- c(rep(0,i),cumsum(ifelse(tmean[i1:tot_day]>11,tmean[i1:tot_day]-11,0)))
+      # ecl_temp is prob of ecolsion on each day if oviposition is on day i
+      ecl_prob <- pnorm(dd11,params$ecl_m,params$ecl_sd) - pnorm(lag(dd11,n=1,default = 0),params$ecl_m,params$ecl_sd)
+      ecl <- ecl + ovi[i] * ecl_prob
+    }
+    
+    sum_sol <- 172 # julian day of summer solstice
+    act_prediapause <- lag(ecl, params$hardening, 0) # hardening time
+    # all ticks eclosing before solstice become active, fraction of htem ecolsing afterwards enter diapause (Ogden et al. 2018)
+    diapause_frac_daily <- c(rep(0, sum_sol + params$hardening), rep(params$diapause, tot_day - (sum_sol + params$hardening)))
+    # fraction of cohort starting activity on each day
+    start_act <- (1 - diapause_frac_daily) * act_prediapause
+    # fraction of cohort that enters diapause after ecolosion
+    diapause_frac_total <- sum(diapause_frac_daily*act_prediapause)
+    
+    # this section calculates the fraction of cohort questing on each day
+    # sums to greater than 1 since a tick can quest on more than one day
+    questing <- rep(0, tot_day)
+    days_to_winter <- min(which(tmean<10)[which(tmean<params$start_quest) > sum_sol]) #here by 'winter' i just mean when temp falls below that which larvae quest
+    active <- 0 # tracks number of active ticks
+    
+    # fraction of ticks active on a given day that will quest based on temp
+    f_quest <- ifelse(tmean < params$max_quest, 
+                      params$quest_slope1*(tmean-params$start_quest),
+                      1 + params$quest_slope1*(tmean-params$max_quest))
+    f_quest <- ifelse(f_quest>1,1,ifelse(f_quest<0,0,f_quest))
+    
+    for (i in 1:days_to_winter)
+    {
+      active <- active + start_act[i] # add number becoming active on day i to alive pool
+      # fraction question on day i 
+      questing[i] <- active * f_quest[i]
+      # remove ticks from active cohort if they find a host or die
+      active <- (1 - params$host_find * f_quest[i] - params$mort) * active
+    }
+    # fraction of the cohort which survives overwinters
+    # overwinter survival times three groups: active at end of questing season; those starting activity after questing season; those that entered diapause earlier
+    larvae_overwinter <- params$overwinter_surv * (active + sum(start_act[(days_to_winter+1):tot_day]) + diapause_frac_total)
+    
+    # add overwintered larvae to questing vector
+    for (i in 1:days_to_winter)
+    {
+      questing[i] <- questing[i] + larvae_overwinter * f_quest[i]
+      larvae_overwinter <- (1 - params$host_find * f_quest[i] - params$mort) * larvae_overwinter
+    }
+    
+    output_tibble <- tibble(
+      ovi = ovi, # fraction of cohort oviposited each day
+      ecl = ecl, # fraction of cohort eclosed each day
+      qst = questing, # fraction of cohort questing each day (sums > 1)
+      qst_norm = questing/sum(questing) # fraction of questing larvae on a given day, for comparison to real data
+    )
+    
+    return(output_tibble)
+  }
+}
 
